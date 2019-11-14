4a9377f24e86696542b6b93f8ba7169bf2598453
Akash Singh <goandsatish@gmail.com>
2019-11-14 22:01:38 +0530

    add app theme

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -11,6 +11,7 @@ class CategoriesScreen extends StatelessWidget {
         title: const Text("DeliMeals"),
       ),
       body: GridView(
+        padding: const EdgeInsets.all(25),
         children: DUMMY_CATEGORIES
             .map((data) => CategoryItem(data.title, data.color))
             .toList(),
