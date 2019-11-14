fade762cc56f4e7b067d3c4a7e7dfb6d1238219d
Akash Singh <goandsatish@gmail.com>
2019-11-14 21:40:07 +0530

    use of home argument of materialApp to set start screen

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -6,15 +6,20 @@ import './dummy_data.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: DUMMY_CATEGORIES
-          .map((data) => CategoryItem(data.title, data.color))
-          .toList(),
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          mainAxisSpacing: 20,
-          crossAxisSpacing: 20,
-          childAspectRatio: 3 / 2),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("DeliMeals"),
+      ),
+      body: GridView(
+        children: DUMMY_CATEGORIES
+            .map((data) => CategoryItem(data.title, data.color))
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,
+            mainAxisSpacing: 20,
+            crossAxisSpacing: 20,
+            childAspectRatio: 3 / 2),
+      ),
     );
   }
 }
