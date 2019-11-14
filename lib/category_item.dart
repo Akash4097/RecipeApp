13424d2e6d78b707b55012c0dd8f3c3921788a44
Akash Singh <goandsatish@gmail.com>
2019-11-14 22:23:45 +0530

    use of Navigator, MaterialPageRoute class

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './category_meal_screen.dart';
 
 class CategoryItem extends StatelessWidget {
   final String title;
@@ -6,17 +7,33 @@ class CategoryItem extends StatelessWidget {
 
   CategoryItem(this.title, this.color);
 
+  void _selectCategory(BuildContext ctx) {
+    Navigator.of(ctx).push(
+      MaterialPageRoute(builder: (_) {
+        return CategoryMealScreen();
+      }),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      child: Text(title, style: Theme.of(context).textTheme.title,),
-      decoration: BoxDecoration(
-          gradient: LinearGradient(
-              colors: [color.withOpacity(0.7), color],
-              end: Alignment.topLeft,
-              begin: Alignment.bottomRight),
-          borderRadius: BorderRadius.circular(15)),
+    return InkWell(
+      onTap: () => _selectCategory(context),
+      borderRadius: BorderRadius.circular(15),
+      splashColor: Theme.of(context).primaryColor,
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.title,
+        ),
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: [color.withOpacity(0.7), color],
+                end: Alignment.topLeft,
+                begin: Alignment.bottomRight),
+            borderRadius: BorderRadius.circular(15)),
+      ),
     );
   }
 }
