a93ee94ab4334defb7e01c19b38dbd6bbdc5f024
Akash Singh <goandsatish@gmail.com>
2019-11-16 13:48:39 +0530

    add BottomNavigationBarItem using bottomNavigation of scaffold

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -3,30 +3,45 @@ import 'package:flutter/material.dart';
 import './categories_screen.dart';
 import './favorites_screen.dart';
 
-class TabsScreen extends StatelessWidget {
+class TabsScreen extends StatefulWidget {
+  @override
+  _TabsScreenState createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  final List<Map<String, Object>> _pages = [
+    {"page": CategoriesScreen(), "title": "Categories"},
+    {"page": FavoritesScreen(), "title": "Favorites"}
+  ];
+
+  int _selectedIndex = 0;
+
+  void _selectedPage(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text("Meals"),
-          bottom: TabBar(
-            tabs: <Widget>[
-              Tab(
-                icon: Icon(Icons.category),
-                text: "Category",
-              ),
-              Tab(
-                icon: Icon(Icons.favorite),
-                text: "Favorites",
-              )
-            ],
-          ),
-        ),
-        body: TabBarView(
-          children: <Widget>[CategoriesScreen(), FavoritesScreen()],
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Meals"),
+      ),
+      body: _pages[_selectedIndex]["page"],
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Theme.of(context).primaryColor,
+        elevation: 4,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.black54,
+        currentIndex: _selectedIndex,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.category), title: Text("Categories")),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.favorite), title: Text("Favorites")),
+        ],
+        onTap: _selectedPage,
       ),
     );
   }
